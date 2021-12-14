3dcf64bd5def60dfe9474893a34c6d8efed09ac4
Roman Shtylman <roman@foxglove.dev>
2021-12-14 12:00:27 -0800

    Use Blob.arrayBuffer to get the array buffer from a blob (#50)
    Rather than the more involved FileReader apis, Blob has an arrayReader method.

--- src/web/BlobReader.ts
+++ src/web/BlobReader.ts
@@ -23,26 +23,8 @@ export default class BlobReader implements Filelike {
 
   // read length (bytes) starting from offset (bytes)
   async read(offset: number, length: number): Promise<Uint8Array> {
-    return await new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onload = function () {
-        reader.onload = null;
-        reader.onerror = null;
-
-        if (reader.result == undefined || !(reader.result instanceof ArrayBuffer)) {
-          reject("Unsupported format for BlobReader");
-          return;
-        }
-
-        resolve(new Uint8Array(reader.result));
-      };
-      reader.onerror = function () {
-        reader.onload = null;
-        reader.onerror = null;
-        reject(reader.error ?? new Error("Unknown FileReader error"));
-      };
-      reader.readAsArrayBuffer(this._blob.slice(offset, offset + length));
-    });
+    const arrBuf = await this._blob.slice(offset, offset + length).arrayBuffer();
+    return new Uint8Array(arrBuf);
   }
 
   // return the size of the file
